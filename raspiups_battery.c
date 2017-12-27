625d6098a7ee113bf80d2661772681a437c1b0e9
lostangel556 <steven.foxton@googlemail.com>
2017-12-27 23:47:00 +0000

    Update raspiups_battery.c

--- raspiups_battery.c
+++ raspiups_battery.c
@@ -1,284 +1,175 @@
 #include <linux/module.h>
-#include <linux/param.h>
-#include <linux/jiffies.h>
-#include <linux/workqueue.h>
-#include <linux/delay.h>
+#include <linux/err.h>
 #include <linux/platform_device.h>
 #include <linux/power_supply.h>
-#include <linux/idr.h>
+#include <linux/types.h>
+#include <linux/pci.h>
+#include <linux/interrupt.h>
+#include <linux/io.h>
 #include <linux/i2c.h>
-#include <asm/unaligned.h>
-#include <linux/gpio.h>
-#include <linux/slab.h>
 
-#define DRIVER_VERSION			"1.0.0"
+#define MAX17048_REG_SOC 0x04
+#define MAX17048_REG_VOLT 0x02
 
-#define BATTERY_CAPACITY_MAH   2500   ///  define battery capacity mah
-#define RSENSE_RESISTANCE		10     ///  Rsense resistance m
 
-#define MAX17048_REG_MODE		0x06
-#define MAX17048_REG_SOC		0x04 // Relative State-of-Charge
-#define MAX17048_REG_ROC 		0x16 //Charge or Discharge Rate
-#define MAX17048_REG_VOLT		0x02 //Current Voltage
-#define MAX17048_REG_STATUS             0x1A // Status
+static struct platform_device *MAX17048_pdev;
+struct i2c_client	*my_i2c_client;
+int AC_ONLINE;
 
-#define MAX17048_SPEED 	100000
-
-struct MAX17048_device_info {
-	struct device 		*dev;
-	struct power_supply	bat;
-	struct delayed_work work;
-	unsigned int interval;
-	struct i2c_client	*client;
-};
-
-static int i2c_master_reg8_send(const struct i2c_client *client, const char reg, const char *buf, int count, int scl_rate)
+static int MAX17048_ac_get_property(struct power_supply *psy,
+		enum power_supply_property psp,
+		union power_supply_propval *val)
 {
-    struct i2c_adapter *adap=client->adapter;
-    struct i2c_msg msg;
-    int ret;
-    char *tx_buf = (char *)kzalloc(count + 1, GFP_KERNEL);
-    if(!tx_buf)
-        return -ENOMEM;
-    tx_buf[0] = reg;
-    memcpy(tx_buf+1, buf, count); 
-    msg.addr = client->addr;
-    msg.flags = client->flags;
-    msg.len = count + 1;
-    msg.buf = (char *)tx_buf;
-    msg.scl_rate = scl_rate;
-    ret = i2c_transfer(adap, &msg, 1); 
-    kfree(tx_buf);
-    return (ret == 1) ? count : ret;
-}
+	int ret = 0;
 
-static int MAX17048_write_regs(struct i2c_client *client, u8 reg, u8 const buf[], __u16 len)
-{
-	int ret; 
-	ret = i2c_master_reg8_send(client, reg, buf, (int)len, MAX17048_SPEED);
+	switch (psp) {
+	case POWER_SUPPLY_PROP_ONLINE:
+		val->intval = AC_ONLINE;
+		break;
+	default:
+		ret = -EINVAL;
+		break;
+	}
 	return ret;
 }
 
-/*
- * Return the battery Voltage in millivolts mV
- * Or < 0 if something fails.
- */
-int battery_charge;
-static int MAX17048_battery_voltage(struct MAX17048_device_info *di)
+static int MAX17048_bat_get_property(struct power_supply *psy,
+				enum power_supply_property psp,
+				union power_supply_propval *val)
 {
-	int ret;
-        long div;
-        div = 78.125;
-        
-        ret = i2c_smbus_read_word_swapped(di->client, MAX17048_REG_VOLT) * div;
-	if (ret<0) {
-		dev_err(di->dev, "MAX17048 error reading voltage\n");
-		return ret;
-	}
-	battery_charge = ret;
-	//printk(KERN_INFO "MAX17048 VOLTAGE: %dmV\n", ret );
-	return ret /1000;   //voltage (uV) = Voltage_code * 78.125
-}
-
-static int MAX17048_battery_capacity(struct MAX17048_device_info *di)
-{
-	int ret;
-        
-        ret = i2c_smbus_read_word_swapped(di->client, MAX17048_REG_SOC) / 256;
-	if (ret<0) {
-		dev_err(di->dev, "MAX17048 error reading capacity\n");
-		return ret;
-	}
-        //printk(KERN_INFO "MAX17048 CAPACITY: %d percent\n", ret);
-	if(ret>100)return 100;
-	else
-	return ret;    ////capacity = swapped hex / 256).
-}
-
-static int dc_charge_status(struct MAX17048_device_info *di)
-{
-         if(MAX17048_battery_voltage(di) > battery_charge)
-		return POWER_SUPPLY_STATUS_CHARGING;
-	else
-		return POWER_SUPPLY_STATUS_DISCHARGING;
+    int ret = 0;
+    int volts;
+    switch (psp) {
+        case POWER_SUPPLY_PROP_STATUS:
+            if (volts < (int)i2c_smbus_read_word_swapped(my_i2c_client, MAX17048_REG_VOLT))
+            {
+            val->intval = POWER_SUPPLY_STATUS_CHARGING;
+            AC_ONLINE = 1;
+            }
+            else{
+            val->intval = POWER_SUPPLY_STATUS_DISCHARGING;
+            AC_ONLINE = 0;
+            }
+            break;
+        case POWER_SUPPLY_PROP_HEALTH:
+            val->intval = POWER_SUPPLY_HEALTH_GOOD;
+            break;
+        case POWER_SUPPLY_PROP_PRESENT:
+            val->intval = 1;
+            break;
+        case POWER_SUPPLY_PROP_TECHNOLOGY:
+            val->intval = POWER_SUPPLY_TECHNOLOGY_LION;
+            break;
+        case POWER_SUPPLY_PROP_CAPACITY:
+            val->intval = (int)i2c_smbus_read_word_swapped(my_i2c_client, MAX17048_REG_SOC) / 256;
+            break;
+        case POWER_SUPPLY_PROP_TEMP:
+            val->intval = 20;
+            break;
+        case POWER_SUPPLY_PROP_VOLTAGE_NOW:
+            volts = (int)i2c_smbus_read_word_swapped(my_i2c_client, MAX17048_REG_VOLT);
+            val->intval = volts;
+            break;
+        default:
+            ret = -EINVAL;
+            break;
+    }
+    return ret;
 }
 
 static enum power_supply_property MAX17048_battery_props[] = {
-	POWER_SUPPLY_PROP_STATUS,
-	POWER_SUPPLY_PROP_PRESENT,
-	12, //POWER_SUPPLY_PROP_VOLTAGE_NOW
-        POWER_SUPPLY_PROP_TECHNOLOGY,
-        POWER_SUPPLY_PROP_HEALTH,
-        39, //POWER_SUPPLY_PROP_CAPACITY
-	53, //POWER_SUPPLY_PROP_TYPE,
-	POWER_SUPPLY_PROP_CHARGE_TYPE,
-	POWER_SUPPLY_PROP_ONLINE,
-	54, //POWER_SUPPLY_PROP_SCOPE
+		POWER_SUPPLY_PROP_STATUS,
+		POWER_SUPPLY_PROP_HEALTH,
+		POWER_SUPPLY_PROP_PRESENT,
+		POWER_SUPPLY_PROP_TECHNOLOGY,
+		POWER_SUPPLY_PROP_VOLTAGE_NOW,
+		POWER_SUPPLY_PROP_TEMP,
+		POWER_SUPPLY_PROP_CAPACITY,
 };
 
-static int MAX17048_battery_get_property(struct power_supply *psy,
-					enum power_supply_property psp,
-					union power_supply_propval *val)
-{
-	struct MAX17048_device_info *di = container_of(psy, struct MAX17048_device_info, bat);
-	switch (psp) {
-	case POWER_SUPPLY_PROP_STATUS:
-		val->intval = dc_charge_status(di);
-		break;
-	case 12: //POWER_SUPPLY_PROP_VOLTAGE_NOW
-		val->intval = MAX17048_battery_voltage(di);
-		break;
-	case POWER_SUPPLY_PROP_PRESENT:
-		val->intval = 1;
-		break;
-        case POWER_SUPPLY_PROP_TECHNOLOGY:
-                val->intval = POWER_SUPPLY_TECHNOLOGY_LIPO;
-                break;
-        case POWER_SUPPLY_PROP_HEALTH:
-                val->intval = POWER_SUPPLY_HEALTH_GOOD;
-                break;
-        case 39: //POWER_SUPPLY_PROP_CAPACITY
-                val->intval = MAX17048_battery_capacity(di);
-                break;
-	case 53: //POWER_SUPPLY_PROP_TYPE:
-		val->intval = 1; //POWER_SUPPLY_TYPE_BATTERY
-		break;
-	case 54: //POWER_SUPPLY_PROP_SCOPE:
-		val->intval = 2; //POWER_SUPPLY_SCOPE_DEVICE
-		break;
-	case POWER_SUPPLY_PROP_CHARGE_TYPE:
-		val->intval = POWER_SUPPLY_CHARGE_TYPE_FAST;
-		break;
-	case POWER_SUPPLY_PROP_ONLINE:
-		val->intval = 4;
-		break;
-	default:
-		return -EINVAL;
-	}
+static enum power_supply_property MAX17048_ac_props[] = {
+		POWER_SUPPLY_PROP_ONLINE,
+};
 
-	return 0;
-}
+static struct power_supply MAX17048_bat = {
+		.name = "battery",
+		.type = POWER_SUPPLY_TYPE_BATTERY,
+		.properties = MAX17048_battery_props,
+		.num_properties = ARRAY_SIZE(MAX17048_battery_props),
+		.get_property = MAX17048_bat_get_property,
+		.use_for_apm = 1,
+};
 
-static void MAX17048_powersupply_init(struct MAX17048_device_info *di)
-{
-	di->bat.type = POWER_SUPPLY_TYPE_BATTERY;
-	di->bat.properties = MAX17048_battery_props;
-	di->bat.num_properties = ARRAY_SIZE(MAX17048_battery_props);
-	di->bat.get_property = MAX17048_battery_get_property;
-	di->bat.external_power_changed = NULL;
-}
-
-static void MAX17048_battery_update_status(struct MAX17048_device_info *di)
-{
-	power_supply_changed(&di->bat);
-}
-
-static void MAX17048_battery_work(struct work_struct *work)
-{
-	struct MAX17048_device_info *di = container_of(work, struct MAX17048_device_info, work.work); 
-
-	MAX17048_battery_update_status(di);
-	/* reschedule for the next time */
-	schedule_delayed_work(&di->work, di->interval);
-}
+static struct power_supply MAX17048_ac = {
+		.name = "ac",
+		.type = POWER_SUPPLY_TYPE_MAINS,
+		.properties =  MAX17048_ac_props,
+		.num_properties = ARRAY_SIZE(MAX17048_ac_props),
+		.get_property = MAX17048_ac_get_property,
+};
 
 static int MAX17048_battery_probe(struct i2c_client *client,
 				 const struct i2c_device_id *id)
 {
-	struct MAX17048_device_info *di;
-	struct platform_device *pd;
-	int retval = 0;
-	u8 regs[2] = {0x06,0x00};  ///init regs mode ctrl
-
-	di = kzalloc(sizeof(*di), GFP_KERNEL);
-	if (!di) {
-		dev_err(&client->dev, "failed to allocate device info data\n");
-		retval = -ENOMEM;
-		goto batt_failed_2;
-	}
-	
-	pd = platform_device_register_simple("battery", 0, NULL, 0);
-
-	i2c_set_clientdata(client, di);
-	di->dev = &client->dev;
-	di->bat.name = "battery";
-	di->bat.use_for_apm = 1;
-	di->client = client;
-	MAX17048_write_regs(client, MAX17048_REG_MODE, regs, 2);
-	/* 4 seconds between monitor runs interval */
-	di->interval = msecs_to_jiffies(4 * 1000);
-	MAX17048_powersupply_init(di);
-
-	retval = power_supply_register(&client->dev, &di->bat);
-	if (retval) {
-		dev_err(&client->dev, "failed to register battery\n");
-		goto batt_failed_3;
-	}
-	
-	INIT_DELAYED_WORK(&di->work, MAX17048_battery_work);
-	schedule_delayed_work(&di->work, di->interval);
-	
-	dev_info(&client->dev, "support ver. %s enabled\n", DRIVER_VERSION);
-
-	return 0;
-
-batt_failed_3:
-	kfree(di);
-batt_failed_2:
-	return retval;
+    my_i2c_client = client;
+    return 0;
 }
-
-static int MAX17048_battery_remove(struct i2c_client *client)
-{
-	struct MAX17048_device_info *di = i2c_get_clientdata(client);
-
-	cancel_delayed_work_sync(&di->work);
-	power_supply_unregister(&di->bat);
-
-	kfree(di->bat.name);
-
-	kfree(di);
-
-	return 0;
-}
-
-/*
- * Module stuff
- */
 
 static const struct i2c_device_id MAX17048_id[] = {
-	{ "MAX17048", 0 },
+	{ "max17048", 0 },
 	{},
 };
 MODULE_DEVICE_TABLE(i2c,MAX17048_id);
 
 static struct i2c_driver MAX17048_battery_driver = {
 	.driver = {
-		.name = "MAX17048",
+		.name = "max17048",
 	},
 	.probe = MAX17048_battery_probe,
-	.remove = MAX17048_battery_remove,
 	.id_table = MAX17048_id,
 };
 
-static int __init MAX17048_battery_init(void)
+static int __init MAX17048_init(void)
 {
-	int ret;
+		int ret = 0;
 
-	ret = i2c_add_driver(&MAX17048_battery_driver);
-	if (ret){
-		printk(KERN_ERR "Unable to register MAX17048 driver\n");}else{
-	printk(KERN_ERR "MAX17048 driver registered\n");}
-	return ret;
+		MAX17048_pdev = platform_device_register_simple("battery", 0, NULL, 0);
+		if (IS_ERR(MAX17048_pdev))return PTR_ERR(MAX17048_pdev);
+                
+                i2c_add_driver(&MAX17048_battery_driver);
+                
+		ret = power_supply_register(&MAX17048_pdev->dev, &MAX17048_bat);
+		if (ret)
+				goto bat_failed;
+
+		ret = power_supply_register(&MAX17048_pdev->dev, &MAX17048_ac);
+		if (ret)
+				goto ac_failed;
+
+		pr_info("MAX17048: android battery driver loaded\n");
+		goto success;
+
+bat_failed:
+		power_supply_unregister(&MAX17048_bat);
+ac_failed:
+		power_supply_unregister(&MAX17048_ac);
+		platform_device_unregister(MAX17048_pdev);
+success:
+		return ret;
 }
-module_init(MAX17048_battery_init);
 
-static void __exit MAX17048_battery_exit(void)
+static void __exit MAX17048_exit(void)
 {
-	i2c_del_driver(&MAX17048_battery_driver);
+		power_supply_unregister(&MAX17048_bat);
+		power_supply_unregister(&MAX17048_ac);
+		platform_device_unregister(MAX17048_pdev);
+                i2c_del_driver(&MAX17048_battery_driver);
+		pr_info("MAX17048: android battery driver unloaded\n");
 }
-module_exit(MAX17048_battery_exit);
+
+module_init(MAX17048_init);
+module_exit(MAX17048_exit);
 
 MODULE_AUTHOR("Lostangel556");
-MODULE_DESCRIPTION("Geekworm Raspberry Pi UPS Hat");
 MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Android MAX17048 battery driver");
